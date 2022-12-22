1138d67a9f36299ab20419a8a80c6af1481d7004
ttsilesh <ttsilesh87gmail.com>
2022-12-22 03:40:39 -0800

    generate password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that generates random password
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, sum = 0, n;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
