23f0383838d96b087a1dd079a5b68bc4a24df6d4
ttsilesh <ttsilesh87gmail.com>
2022-12-22 22:31:48 -0800

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,29 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-	n1 = n;
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar('-');
-		n1 = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			positive = n * -1;
+		}
+
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints 98\n
- * Return: 0
+ * main - prints 98 followed by new line
+ * Return: Always 0
  */
 
 int main(void)
