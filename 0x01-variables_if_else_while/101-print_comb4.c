43db3504744c3204ca5030a53d797d5717e8aabe
ttsilesh <ttsilesh87gmail.com>
2023-01-08 01:29:33 -0800

    combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n, i, h;
+
+	for (h = 0 ; h != 8; n++)
+	{
+		for (n = h + 1 ; n != 9 ; n++)
+		{
+		i = n + 1;
+		do {
+			putchar('0' + h);
+			putchar('0' + n);
+			putchar('0' + i);
+			if (n != 7)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			i++;
+		} while (i != 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
