7e59c16afae8a955b27ff21348a34a1a5b201aac
ttsilesh <ttsilesh87gmail.com>
2023-01-08 00:48:04 -0800

    print combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n, i;
+
+	for (n = 0 ; n < 9; n++)
+	{
+		i = n + 1;
+		do {
+			putchar('0' + n);
+			putchar('0' + i);
+			if (n < 8)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			i++;
+		} while (i < 10);
+	}
+	putchar('\n');
+	return (0);
+}
