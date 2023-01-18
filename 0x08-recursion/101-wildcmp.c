fc11e22f142b81bed6eb34294d889a8ecd60ca30
ttsilesh <ttsilesh87gmail.com>
2023-01-18 11:26:04 -0800

    function pointer

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - Entry Point
+ * @s1: input
+ * @s2: input
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-low level programming
+alx-low_level_programming
