21fd042abbfe0b6ccb510674b2e4887813435443
ttsilesh <ttsilesh87gmail.com>
2023-01-22 08:56:58 -0800

    last commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -20,14 +19,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-
+	
 	int i = 0;
 
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (0);
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,33 +10,29 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+		 printf("Error\n");
+		 exit(98);
 	}
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3])
-
-	func = get_op_func(argv[2]);
-	if (!func)
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	o = *argv[2];
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = func(arg1, arg2);
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
